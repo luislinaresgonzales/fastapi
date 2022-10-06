af4ddc1119c45387cf8c3b84649274189e3704aa
Luis Linares <luuchhoo@gmail.com>
2022-10-06 17:11:26 -0400

    requare_response body

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
+#Python
+from typing import Optional
+
+#Pydantic
+from pydantic import BaseModel
+
+#fastapi
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
+
+#Models
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
 
 @app.get("/")
 def home():
     return {"hello" : "work"}
+
+#request and response body
+
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return person    
