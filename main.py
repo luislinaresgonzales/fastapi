257a59050f976d42f9250b1ad6cfc7c7a323337e
Luis Linares <luuchhoo@gmail.com>
2022-10-08 00:07:27 -0400

    automatic pat and query example

--- main.py
+++ main.py
@@ -77,12 +77,14 @@ def show_person(
         min_length=1, 
         max_length=50,
         title="Person Name",
-        description="This is the person name. It's between 1 and 50 characters"
+        description="This is the person name. It's between 1 and 50 characters",
+        example="Rocio"
         ),
     age: str = Query(
         ...,
         title="Person Age",
-        description="This is the person age, It's required"
+        description="This is the person age, It's required",
+        example="25"
         )
 ):   
     return {name: age}
@@ -90,7 +92,11 @@ def show_person(
 #validation path param
 @app.get("/person/detail/{person_id}")
 def show_person(
-    person_id: int = Path(..., gt=0)
+    person_id: int = Path(
+        ..., 
+        gt=0,
+        example=123
+        )
 ):
     return {person_id: "It exists!!"}   
 
@@ -101,7 +107,8 @@ def update_person(
         ...,
         title="Person ID",
         description="This is the person ID",
-        gt=0
+        gt=0,
+        example=123
     ),
     person: Person = Body(...),
     location: Location = Body(...)
