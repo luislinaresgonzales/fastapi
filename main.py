2e8d481748a5ad52060ae3fc2b1769de1b814d03
Luis Linares <luuchhoo@gmail.com>
2022-10-08 21:08:23 -0400

    response model

--- main.py
+++ main.py
@@ -45,7 +45,30 @@ class Person(BaseModel):
     )
     hair_color: Optional[HairColor] = Field(default=None, example="black")
     is_married: Optional[bool] = Field(default=None, example=False)
-    
+    password: str = Field(..., min_length=8)
+
+class PersonOut(BaseModel):
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        example="Luis"
+        )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        example="Linares"
+        )
+    age: int = Field(
+        ...,
+        gt=0,
+        le=115,
+        example="31"
+    )
+    hair_color: Optional[HairColor] = Field(default=None, example="black")
+    is_married: Optional[bool] = Field(default=None, example=False)
+
     # class Config:
     #     schema_extra = {
     #         "example" : {
@@ -63,7 +86,7 @@ def home():
 
 #request and response body
 
-@app.post("/person/new")
+@app.post("/person/new", response_model=PersonOut)
 def create_person(person: Person = Body(...)):
     return person    
 
